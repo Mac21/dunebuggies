e5cac2699ba77a55e2f73335961b9f18ae428f63
Russ Tolle <russ.tolle@gmail.com>
2025-01-24 09:43:22 -0700

    Add default params for fill color and selected_color to menu.

--- menu.hpp
+++ menu.hpp
@@ -1,37 +1,46 @@
 #pragma once
 
-class Menu {
-public:
-    Menu(sf::Font& font) : font(font) {
-        options = {"Single Player", "Single Player with Bots", "Host", "Join", "Exit"};
-        for (auto& option : options) {
-            sf::Text text(font, option, 24);
-            text.setFillColor(sf::Color::White);
-            texts.push_back(std::move(text));
+namespace db {
+    class Menu {
+    public:
+        Menu(sf::Font& font, sf::Color fill_color = sf::Color::White, sf::Color selected_color = sf::Color::Black) : font(font),
+                                                                                                                   fill_color(fill_color),
+                                                                                                                   selected_color(selected_color) {
+            options = { "Single Player", "Single Player with Bots", "Host", "Join", "Exit" };
+            for (auto& option : options) {
+                sf::Text text(font, option, 24);
+                text.setFillColor(fill_color);
+                texts.push_back(std::move(text));
+            }
+            selected = 0;
+            updateTextPositions();
         }
-        selected = 0;
-        updateTextPositions();
-    }
 
-    void draw(sf::RenderWindow& window) {
-        for (size_t i = 0; i < texts.size(); ++i) {
-            texts[i].setFillColor(i == selected ? sf::Color::Red : sf::Color::White);
-            window.draw(texts[i]);
+        void draw(sf::RenderWindow& window) {
+            for (size_t i = 0; i < texts.size(); ++i) {
+                if (i == selected) {
+                    texts[i].setStyle(sf::Text::Bold | sf::Text::Underlined);
+                    texts[i].setFillColor(selected_color);
+                } else {
+                    texts[i].setStyle(sf::Text::Regular);
+                    texts[i].setFillColor(fill_color);
+                }
+                window.draw(texts[i]);
+            }
         }
-    }
 
-    bool isVisible() const {
-        return !is_hidden;
-    }
+        bool isVisible() const {
+            return !is_hidden;
+        }
 
-    void hide(bool yn) {
-        is_hidden = yn;
-    }
+        void hide(bool yn) {
+            is_hidden = yn;
+        }
 
-    void handleInput(std::optional<sf::Event> event) {
-        if (event->is<sf::Event::KeyPressed>()) {
-            auto kpe = event->getIf<sf::Event::KeyPressed>();
-            switch (kpe->code) {
+        void handleInput(std::optional<sf::Event> event) {
+            if (event->is<sf::Event::KeyPressed>()) {
+                auto kpe = event->getIf<sf::Event::KeyPressed>();
+                switch (kpe->code) {
                 case sf::Keyboard::Key::Up: {
                     if (selected > 0) {
                         selected--;
@@ -55,31 +64,34 @@ public:
                     }
                     break;
                 }
+                }
             }
         }
-    }
 
-    void setOnSelect(std::function<void(int)> callback) {
-        onSelect = callback;
-    }
-
-    sf::Vector2f getFirstTextPosition() {
-        return texts[0].getPosition();
-    }
-
-private:
-    void updateTextPositions() {
-        float y = 200.0f;
-        for (auto& text : texts) {
-			text.setPosition({ 400, y });
-			y += 50.0f;
+        void setOnSelect(std::function<void(int)> callback) {
+            onSelect = callback;
         }
-    }
 
-    bool is_hidden;
-    sf::Font& font;
-    std::vector<std::string> options;
-    std::vector<sf::Text> texts;
-    size_t selected;
-    std::function<void(int)> onSelect;
-};
+        sf::Vector2f getFirstTextPosition() {
+            return texts[1].getPosition();
+        }
+
+    private:
+        void updateTextPositions() {
+            float y = 300.0f;
+            for (auto& text : texts) {
+                text.setPosition({ 500, y });
+                y += 50.0f;
+            }
+        }
+
+        sf::Color fill_color;
+        sf::Color selected_color;
+        bool is_hidden;
+        sf::Font& font;
+        std::vector<std::string> options;
+        std::vector<sf::Text> texts;
+        size_t selected;
+        std::function<void(int)> onSelect;
+    };
+}
